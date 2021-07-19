bb729262417e1fb603e01fb370f2238a80e2456b
Cherish0207 <cheshire_1207@foxmail.com>
2021-07-19 10:35:55 +0800

    截流实现：3.两者结合，传递第三个参数immediate

--- 函数节流：throttle/3.两者结合，传递第三个参数immediate.js
+++ 函数节流：throttle/3.两者结合，传递第三个参数immediate.js
@@ -1,19 +1,8 @@
-/**
- * 函数节流:throttle
- * 保证函数在一个时间段内只执行一次,有2种做法
- * 1.等时间到了执行第一次
- * 2.立即执行一次再说
- */
-
-window.onresize = throttle((e) => {
+const handler = throttle((e) => {
   console.log(e);
 }, 1000);
+window.onresize = handler;
 
-/**
- * 实现:
- * 如果发现当前已经有计时了，啥都不做
- * 执行一次后timer必须清空，不然永远不会执行下一次了
- */
 function throttle(callback, time, immediate) {
   let timer, timer1;
   return function () {
@@ -33,8 +22,4 @@ function throttle(callback, time, immediate) {
       }, time);
     }
   };
-}
-
-for (let i = 0; i < 1000; i++) {
-  // handler(i); // 连续调用handle函数，在1000毫秒内也只会执行一次。
 }
